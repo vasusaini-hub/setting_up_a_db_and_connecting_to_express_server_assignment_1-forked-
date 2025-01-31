2d787d07f9266c33ba7ccfc15b780e33773d5da3
vasu saini <vasusaini820@gmail.com>
2025-01-31 10:28:35 +0530

    db assignment

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const { resolve } = require('path');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
 
-app.get('/', (req, res) => {
+dotenv.config();
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose 
+  .connect(MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(()=>{
+    console.log('Connected to MongoDB');
+  })
+  .catch((err)=>{
+    console.error("Error connecting t MongoDB");
+  });
+
+  app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
